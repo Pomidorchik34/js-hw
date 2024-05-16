b48c48dd27d5ca31b2135c8b1c516d1a4d6b5ae8
Pomidorchik34 <dimikabd95@gmail.com>
2024-05-16 14:44:27 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+// console.log("Hello world");
+// var password = "123";
+// alert(`hello `);
+// let name = prompt(`what's your name?`);
+// confirm(`${name} is it?`);
+// document.write("salom ", name);
+
+// 1
+// let uzunlik = 1000;
+// let resault = uzunlik / 100;
+// console.log(`${resault}m`);
+
+// 2
+// let ogirlik = 1000;
+// let resault = ogirlik / 1000;
+// console.log(`${resault}t`);
+
+// 3
+// let hajm = 2048;
+// let resault = hajm / 1024;
+// console.log(`${resault}kb`);
+
+// 4
+// let num1 = 2;
+// let num2 = 5;
+// let a;
+// let b;
+// if (num1 > num2) {
+//   a = num1;
+//   b = num2;
+// } else {
+//   b = num1;
+//   a = num2;
+// }
+// console.log(`${a / b} marta joylashadi `);
+
+// 5
+// let num1 = 2;
+// let num2 = 5;
+// let a;
+// let b;
+// if (num1 > num2) {
+//   a = num1;
+//   b = num2;
+// } else {
+//   b = num1;
+//   a = num2;
+// }
+// console.log(`${a % b} sigmadi`);
+
+// 6
+// let n = 23;
+// console.log((n % 100) - (n % 10), n % 10);
+
+// 7
+// let n = 24;
+// console.log((n % 10) + (n - (n % 10)) / 10);
+
+// 8
+// let n = 24;
+// console.log(n % 10, (n - (n % 10)) / 10);
+
+// 9
+// let n = 123;
+// console.log(((n % 1000) - (n % 100)) / 100);
+
+// 10
+// let n = 123;
+// console.log(
+//   ((n % 1000) - (n % 100)) / 100,
+//   ((n % 100) - (n % 10)) / 10,
+//   n % 10
+// );
+
+// 11
+// let n = 123;
+// console.log(
+//   ((n % 1000) - (n % 100)) / 100 + ((n % 100) - (n % 10)) / 10 + (n % 10)
+// );
+
+// 12
+// let n = 123;
+// console.log(
+//   n % 10,
+//   ((n % 100) - (n % 10)) / 10,
+//   ((n % 1000) - (n % 100)) / 100
+// );
+
+// 13
+// let n = 123;
+// console.log(
+//   ((n % 100) - (n % 10)) / 10,
+//   n % 10,
+//   ((n % 1000) - (n % 100)) / 100
+// );
