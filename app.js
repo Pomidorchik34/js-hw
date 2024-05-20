fa0311fe9f5ab974436e00ff5a63d277793c70a5
Pomidorchik34 <dimikabd95@gmail.com>
2024-05-20 14:12:50 +0300

    12

--- app.js
+++ app.js
@@ -103,7 +103,7 @@
 //     num += 1;
 // }
 
-// // Natijani chiqarish
+//
 // console.log("Natija: " + num);
 
 // 2
@@ -117,7 +117,7 @@
 //     num -= 2;
 // }
 
-// // Natijani chiqarish
+//
 // console.log("Natija: " + num);
 
 // 3
@@ -133,7 +133,7 @@
 //     num = 10;
 // }
 
-// // Natijani chiqarish
+//
 // console.log("Natija: " + num);
 
 // 4
@@ -157,7 +157,7 @@
 //     musbatnumlarnumi++;
 // }
 
-// // Natijani chiqarish
+//
 // console.log("Uchta kiritilgan numlar orasida " + musbatnumlarnumi + " ta musbat num bor.");
 
 // 5
@@ -188,7 +188,7 @@
 //     manfiynumlarnumi++;
 // }
 
-// // Natijani chiqarish
+//
 // console.log("Uchta kiritilgan numlar orasida " + musbatnumlarnumi + " ta musbat num va " + manfiynumlarnumi + " ta manfiy num bor.");
 
 // 6
@@ -205,7 +205,7 @@
 //     kattanum = num2;
 // }
 
-// // Natijani chiqarish
+//
 // console.log("Katta num: " + kattanum);
 
 // 7
@@ -213,7 +213,7 @@
 // let num1 = parseInt(prompt("1-numni kiriting:"));
 // let num2 = parseInt(prompt("2-numni kiriting:"));
 
-// // Kiritilgan numlarni solishtirish
+//
 // let kattanum;
 // let kichiknum;
 
@@ -233,7 +233,7 @@
 // let A = +prompt("A numni kiriting:");
 // let B = +prompt("B numni kiriting:");
 
-// // numlarni solishtirish
+//
 // if (A > B) {
 //   console.log("A:", A);
 //   console.log("B:", B);
@@ -243,11 +243,11 @@
 // }
 
 // 9
-// // Foydalanuvchidan ikkita butun num kiritish
+//
 // let A = parseInt(prompt("A numni kiriting:"));
 // let B = parseInt(prompt("B numni kiriting:"));
 
-// // numlarni solishtirish va o'zlashtirish
+//
 // if (A != B) {
 //   A = A + B;
 //   B = A;
@@ -260,11 +260,10 @@
 // console.log("B:", B);
 
 // 10
-// // Foydalanuvchidan ikkita butun num kiritish
+//
 // let A = parseInt(prompt("A numni kiriting:"));
 // let B = parseInt(prompt("B numni kiriting:"));
 
-// // numlarni solishtirish va o'zlashtirish
 // if (A != B) {
 //     if (A > B) {
 //         B = A;
@@ -301,11 +300,9 @@
 // let num1 = +prompt("1-numni kiriting:");
 // let num2 = +prompt("2-numni kiriting:");
 // let num3 = +prompt("3-numni kiriting:");
-
-// // O'rta numni aniqlash
 // let ortanum;
 
-// // numlarni solishtirish
+//
 // if ((num1 >= num2 && num1 <= num3) || (num1 <= num2 && num1 >= num3)) {
 //     ortanum = num1;
 // } else if ((num2 >= num1 && num2 <= num3) || (num2 <= num1 && num2 >= num3)) {
@@ -321,10 +318,10 @@
 // let num2 = +prompt("2-numni kiriting:");
 // let num3 = +prompt("3-numni kiriting:");
 
-// // Kichik va katta numlarni aniqlash
+//
 // let kichiknum, kattanum;
 
-// // numlarni solishtirish
+//
 // if (num1 <= num2 && num1 <= num3) {
 //     kichiknum = num1;
 //     if (num2 >= num3) {
